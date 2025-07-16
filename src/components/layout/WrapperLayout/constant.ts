c281f7ec5f389cdb496cbfc4d891168e194024bc
AvavSam <avav2608005@gmail.com>
2025-07-16 23:45:17 +0800

    feat: move authPaths to a separate constant file

---
+++ src/components/layout/WrapperLayout/constant.ts
@@ -0,0 +1,1 @@
+export const authPaths = ["sign-in", "sign-up", "forgot-password", "reset-password"];
--- src/components/layout/WrapperLayout/index.tsx
+++ src/components/layout/WrapperLayout/index.tsx
@@ -5,11 +5,11 @@ import Footer from "@/components/layout/Footer";
 import { AuthProvider } from "@/components/provider/AuthProvider";
 import { ThemeProvider } from "@/components/provider/ThemeProvider";
 import { useParams, usePathname } from "next/navigation";
+import { authPaths } from "./constant";
 
 const WrapperLayout = ({ children }: { children: React.ReactNode }) => {
   const params = useParams();
   const pathname = usePathname();
-  const authPaths = ["sign-in", "sign-up", "forgot-password", "reset-password"];
   const isAuthPage = authPaths.some((path) => pathname.includes(path));
   const isCertificateDetailPage = !!params?.slug && pathname.includes("certificates");
 
