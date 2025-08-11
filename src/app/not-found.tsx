b5599bee10bd4461215b666fceaf6985f2febd19
mpsalunggg <putrasatria893@gmail.com>
2025-08-11 23:47:29 +0700

    chore: remove comment on notfound page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,17 +1,11 @@
 "use client";
 
 import NotFoundPage from "@/components/layout/NotFoundPage";
-// import Error from "next/error";
-
-// This page renders when a route like `/unknown.txt` is requested.
-// In this case, the layout at `app/[locale]/layout.tsx` receives
-// an invalid value as the `[locale]` param and calls `notFound()`.
 
 export default function GlobalNotFound() {
   return (
     <html lang="en">
       <body suppressHydrationWarning className="flex h-screen w-full items-center justify-center">
-        {/* <Error statusCode={404} />; */}
         <NotFoundPage />
       </body>
     </html>
